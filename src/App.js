baf548c5960c7ffacc1626fa95234e419e682bb0
ybustamante <Think*25>
2021-05-01 19:22:18 -0400

    change app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a test <code>React</code> on docker.
         </p>
         <a
           className="App-link"
